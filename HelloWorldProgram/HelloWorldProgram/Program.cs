3e046d7dafa7858feca7c78994f3eee61c150ed4
Michael Kay <mickay@amazon.com>
2016-06-28 20:04:41 -0700

    These are a couple of new projects for testing in class.

---
+++ HelloWorldProgram/HelloWorldProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+            if (10 > 3)
+            {
+                Console.WriteLine("meow");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Woof");
+                Console.Read();
+            }
+        }
+    }
+}
